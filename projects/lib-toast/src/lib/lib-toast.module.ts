7c65242ef22a1204b85b15227fc290eed3224a29
Stephane Eybert <mittiprovence@yahoo.se>
2020-05-10 15:53:57 +0200

    Updated the import statements so as to use the newly added local dependencies.

--- projects/lib-toast/src/lib/lib-toast.module.ts
+++ projects/lib-toast/src/lib/lib-toast.module.ts
@@ -2,7 +2,7 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { ToastComponent } from './toast.component';
 import { defaultToastConfig, TOAST_CONFIG_TOKEN } from './toast-config';
-import { LibI18nModule } from 'lib-i18n';
+import { LibI18nModule } from '@stephaneeybert/lib-i18n';
 
 @NgModule({
   declarations: [
